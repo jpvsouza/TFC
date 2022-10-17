93b03385c7c0d37431035af8f58050cad57ca4c5
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-10-17 14:58:48 -0300

    feat(Matches): :sparkles: Get by id model implemented

--- app/backend/src/models/MatchModel.ts
+++ app/backend/src/models/MatchModel.ts
@@ -5,4 +5,6 @@ export default class MatchModel {
   private _sequelizeModel = Match;
 
   public getAll = async (): Promise<Match[]> => this._sequelizeModel.findAll(matchIncudeTeams);
+
+  public getById = async (id: number): Promise<Match | null> => this._sequelizeModel.findByPk(id);
 }
