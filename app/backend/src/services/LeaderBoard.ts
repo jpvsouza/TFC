3a5faf683064757c64e62340c18e81692063adaa
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-10-18 15:55:27 -0300

    feat(Service): :sparkles: Leaderboard Service created

---
+++ app/backend/src/services/LeaderBoard.ts
@@ -0,0 +1,36 @@
+import TeamStatus from '../dtos/TeamStatus';
+import MatchModel from '../models/MatchModel';
+import TeamModel from '../models/TeamModel';
+
+export default class LeaderBoardService {
+  private _teamModel = new TeamModel();
+  private _matchModel = new MatchModel();
+
+  public getResults = async (): Promise<TeamStatus[]> => {
+    const teams = await this._teamModel.getAll();
+    const matches = await this._matchModel.getAll();
+
+    return teams.map(
+      (team) =>
+        new TeamStatus(
+          team,
+          matches.filter((match) => !match.inProgress),
+        ),
+    );
+  };
+
+  public getHomeResults = async (): Promise<TeamStatus[]> => {
+    const teams = await this._teamModel.getAll();
+    const matches = await this._matchModel.getAll();
+
+    return teams.map(
+      (team) =>
+        new TeamStatus(
+          team,
+          matches
+            .filter((match) => !match.inProgress)
+            .filter((match) => match.homeTeam === team.id),
+        ),
+    );
+  };
+}
