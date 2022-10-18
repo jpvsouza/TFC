a6fe9b8b4dbfde77c1055dbe3c9c5b63c6790313
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-10-18 16:47:35 -0300

    feat(Router): :sparkles: Leaderboard router created

---
+++ app/backend/src/router/leaderboardRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import LeaderboardController from '../controllers/LeaderboardController';
+
+const router = Router();
+
+const leaderboardController = new LeaderboardController();
+
+router.get('/', leaderboardController.getResults);
+router.get('/home', leaderboardController.getHomeResults);
+router.get('/away', leaderboardController.getAwayResults);
+
+export default router;
