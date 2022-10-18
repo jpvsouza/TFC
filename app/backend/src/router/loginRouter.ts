6c71a768fbb6d417165ccc9951b77f33fffb9a2b
Joao Paulo Veloso <joaopaulox@outlook.com>
2022-10-18 14:09:24 -0300

    refactor(Middlewares): :bug: Authentication middleware updated to fix bugs

--- app/backend/src/router/loginRouter.ts
+++ app/backend/src/router/loginRouter.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import ValidaLogin from '../middlewares/validaLogin';
 import LoginControler from '../controllers/loginController';
 import authenticate from '../middlewares/auth';
 
 const router = Router();
 const loginController = new LoginControler();
 
-router.post('/', ValidaLogin, loginController.login);
-router.get('/validate', authenticate, loginController.validade);
+router.post('/', loginController.login);
+router.get('/validate', authenticate, loginController.validate);
 
 export default router;
